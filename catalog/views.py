eba3b5f6a629d48326dda6bd469d57dcfd254698
grigilo <tsoy.grigoriy@gmail.com>
2024-04-11 13:06:54 +0300

    Homework 19.2

---
+++ catalog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+
+    return render(request, 'catalog/index.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        print(f'{name} ({email}): {message}')
+    return render(request, 'catalog/contact.html')
+
+
+
+
