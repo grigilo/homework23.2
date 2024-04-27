8123f4f2f9a0f96f6722be512614e4d4d3598b3b
grigilo <tsoy.grigoriy@gmail.com>
2024-04-27 12:00:26 +0300

    HW 21.1 Tasks 1,2,3

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
+
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Категория')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', **NULLABLE,
+                              verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateField(auto_now_add=True,
+                                  verbose_name='Дата создания')
+    updated_at = models.DateField(auto_now=True,
+                                  verbose_name='Дата последнего изменения')
+    # manufactured_at = models.DateField(**NULLABLE,
+    #                                    verbose_name="Дата производства")
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catalog',
+        'USER': 'postgres',
+        'PASSWORD': 'Pjkeirf8$'
     }
 }
 
@@ -118,9 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     BASE_DIR / 'static'
-]
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
