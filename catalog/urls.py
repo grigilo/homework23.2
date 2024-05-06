763bf7aead5ba61055af6829995e95194ee77583
grigilo <tsoy.grigoriy@gmail.com>
2024-05-06 13:09:50 +0300

    HW 20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from catalog.views import product_list, product_detail
 
+app_name = 'catalog'
+
 urlpatterns = [
     path('', product_list, name='product_list'),
     path('catalog/<int:pk>/', product_detail, name='product_detail'),
