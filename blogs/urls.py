6fd934b2971a84194705c282f589a82345971621
grigilo <tsoy.grigoriy@gmail.com>
2024-05-14 13:23:21 +0300

    HW 21.1 second task done

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from blogs.apps import BlogsConfig
-from blogs.views import BlogCreateView
+from blogs.views import BlogCreateView, BlogListView, BlogDetailView, \
+    BlogUpdateView, BlogDeleteView
 
 app_name = BlogsConfig.name
 
 
 urlpatterns = [
     path('create/', BlogCreateView.as_view(), name='create'),
-    # path(' ', ..., name='list'),
-    # path('view/<int:pk>/', ..., name='view'),
-    # path('edit/<int:pk>/', ..., name='edit'),
-    # path('delete/<int:pk>/', ..., name='delete'),
+    path(' ', BlogListView.as_view(), name='list'),
+    path('view/<int:pk>/', BlogDetailView.as_view(), name='view'),
+    path('edit/<int:pk>/', BlogUpdateView.as_view(), name='edit'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, \
+    DeleteView
 
 from blogs.models import Blog
 
@@ -9,5 +10,24 @@ from blogs.models import Blog
 # Create your views here.
 class BlogCreateView(CreateView):
     model = Blog
-    fields = ('title', 'content', )
-    success_url = reverse_lazy('main:index')
+    fields = ('title', 'content',)
+    success_url = reverse_lazy('blogs:list')
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'content',)
+    success_url = reverse_lazy('blogs:list')
+
+
+class BlogListView(ListView):
+    model = Blog
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blogs:list')
