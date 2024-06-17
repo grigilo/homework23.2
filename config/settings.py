c9eb2e00cd3fa0fc7a4161561e504680b6e9839f
grigilo <tsoy.grigoriy@gmail.com>
2024-06-17 13:41:53 +0300

    HW 23.2 1 Task

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,10 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/users/'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
